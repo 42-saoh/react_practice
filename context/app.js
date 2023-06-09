a06cc3857665bc94cc9e0b8dc6ce6f265a4e14c6
saoh <saoh@student.42seoul.kr>
2023-06-09 16:20:11 +0900

    [feat] add context

---
+++ context/app.js
@@ -0,0 +1,40 @@
+import React, { createContext } from "react";
+
+const UserInfo = createContext({ name: "gary", id: "garyIsFree" });
+
+const App = () => {
+  return (
+      <HelloLicat />
+  );
+};
+
+const HelloLicat = () => {
+  return (
+    <UserInfo.Consumer>
+      {(value) => (
+        <div>
+          <h2>{value.name}</h2>
+          <strong>{value.id}</strong>
+            <UserInfo.Provider value={{ name: "Licat", id: "ImLion" }}>
+              <HelloLicatTwo/>
+            </UserInfo.Provider>
+        </div>
+      )}
+    </UserInfo.Consumer>
+  );
+};
+
+const HelloLicatTwo = () => {
+  return (
+    <UserInfo.Consumer>
+      {(value) => (
+        <div>
+          <h2>{value.name}</h2>
+          <strong>{value.id}</strong>
+        </div>
+      )}
+    </UserInfo.Consumer>
+  );
+};
+
+export default App;
